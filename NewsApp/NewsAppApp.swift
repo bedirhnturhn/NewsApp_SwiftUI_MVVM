8a37b918aa11c793eb0a5ed795d89d10aa936d4d
Bedirhan Turhan <extern.bedirhan-turhan@allianz.com.tr>
2023-10-08 17:20:01 +0300

    added xcode project

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Bedirhan Turhan on 8.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
